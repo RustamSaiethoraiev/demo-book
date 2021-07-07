61afca970aa69a2397e37f1f489d92dde253e9b7
Rustam Saiethoraiev <rustam1822@ukr.net>
2021-07-07 18:46:41 +0300

    edit heading

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -17,55 +17,56 @@ const EditBook = (props) => {
     }
     return (
         <div className="container">
+            <h2>Edit book</h2>
+            <FormGroup>
+                <Label for="title">Book title</Label>
+                <Input
+                    required
+                    type="text"
+                    id="title"
+                    placeholder="title"
+                    value={newBookData.title}
+                    onChange={(e) => setNewBookData({ ...newBookData, title: e.target.value })} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="autor">Autor name</Label>
+                <Input
+                    required
+                    type="text"
+                    id="autor"
+                    placeholder="autor name"
+                    value={newBookData.autor}
+                    onChange={(e) => setNewBookData({ ...newBookData, autor: e.target.value })} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="exampleSelect">Category</Label>
+                <Input
+                    required
+                    type="select"
+                    name="select"
+                    id="exampleSelect"
+                    value={newBookData.category}
+                    onChange={(e) => setNewBookData({ ...newBookData, category: e.target.value })} >
+                    <option value=""></option>
+                    <option value="Detective">Detective</option>
+                    <option value="Novel">Novel</option>
+                    <option value="Adventures">Adventures</option>
+                </Input>
+            </FormGroup>
+            <FormGroup>
+                <Label for="id">ISBN</Label>
+                <Input
+                    required
+                    type="number"
+                    id="id"
+                    placeholder="id"
+                    value={newBookData.id}
+                    onChange={(e) => setNewBookData({ ...newBookData, id: e.target.value })} />
+            </FormGroup>
+            <Button className="m-2" >Update</Button>
             <Link to="/">
                 <Button color="primary">Dashboard</Button> </Link>
             <Form onSubmit={update}>
-                <FormGroup>
-                    <Label for="title">Book title</Label>
-                    <Input
-                        required
-                        type="text"
-                        id="title"
-                        placeholder="title"
-                        value={newBookData.title}
-                        onChange={(e) => setNewBookData({ ...newBookData, title: e.target.value })} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="autor">Autor name</Label>
-                    <Input
-                        required
-                        type="text"
-                        id="autor"
-                        placeholder="autor name"
-                        value={newBookData.autor}
-                        onChange={(e) => setNewBookData({ ...newBookData, autor: e.target.value })} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelect">Category</Label>
-                    <Input
-                        required
-                        type="select"
-                        name="select"
-                        id="exampleSelect"
-                        value={newBookData.category}
-                        onChange={(e) => setNewBookData({ ...newBookData, category: e.target.value })} >
-                        <option value=""></option>
-                        <option value="Detective">Detective</option>
-                        <option value="Novel">Novel</option>
-                        <option value="Adventures">Adventures</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="id">ISBN</Label>
-                    <Input
-                        required
-                        type="number"
-                        id="id"
-                        placeholder="id"
-                        value={newBookData.id}
-                        onChange={(e) => setNewBookData({ ...newBookData, id: e.target.value })} />
-                </FormGroup>
-                <Button className="m-2" >Update</Button>
             </Form>
         </div >
     );
